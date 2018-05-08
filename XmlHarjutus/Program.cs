ddfc69e1e429c7ca4d54827a7497d2ca4ed86b4a
Virk0 <virko.veelmaa@tptlive.ee>
2018-05-08 09:08:17 +0300

    Lisasin vajadusi juurde
    -Pealkiri peab olema vähemalt 2 tähte pikk
    -Pealkiri ei tohi olla pikem kui 12 tähte
    -Pealkiri ei tohi olla tühi
    -Note peab olema alla 140 tähe

--- XmlHarjutus/Program.cs
+++ XmlHarjutus/Program.cs
@@ -32,28 +32,108 @@ namespace XmlHarjutus
                     {
                         Console.WriteLine("Enter Your desired title for note!");
                         NoteTitle.Value = Console.ReadLine();
-                        if (NoteTitle.Value.Contains("  "))
+                        //if (!string.IsNullOrEmpty(NoteTitle.Value))
+                        //{
+                        //    SomeFunction(NoteTitle.Value);
+                        //}
+                        //if (!string.IsNullOrEmpty(NoteTitle.Value))
+                        //{
+                        //    while (true)
+                        //    {
+                        //        Console.WriteLine("Title can't be empty");
+                        //        Console.WriteLine("Enter Your desired title for note!");
+                        //        NoteTitle.Value = Console.ReadLine();
+                        //        if (!string.IsNullOrEmpty(NoteTitle.Value))
+                        //        {
+                        //            Console.WriteLine("Title can't be empty");
+                        //            Console.WriteLine("Enter Your desired title for note!");
+                        //            NoteTitle.Value = Console.ReadLine();
+                        //            if (!string.IsNullOrEmpty(NoteTitle.Value))
+                        //            {
+
+                        //            }
+                        //        }
+                        //        break;
+                        //    }
+                        //}
+                        if (NoteTitle.Value == "" || NoteTitle.Value == " " || NoteTitle.Value == "  " || NoteTitle.Value == "   " || NoteTitle.Value == "   " )
                         {
-                            Console.WriteLine("Title can't be empty");
+                            Console.WriteLine("Note can't be empty");
+                        }
+                        if (NoteTitle.Value.Length <= 1)
+                        {
+                            while (true)
+                            {
+                                Console.WriteLine("Title must be atleast 2 letters long!");
+                                Console.WriteLine("Enter Your desired title for note!");
+                                NoteTitle.Value = Console.ReadLine();
+                                if (NoteTitle.Value.Length <= 1)
+                                {
+
+                                }
+                                if (NoteTitle.Value.Length >= 1)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        if (NoteTitle.Value.Length >= 13)
+                        {
+                            while (true)
+                            {
+                                Console.WriteLine("Title can't be longer than 12 letters");
+                                Console.WriteLine("Enter Your desired title for note!");
+                                NoteTitle.Value = Console.ReadLine();
+                                if (NoteTitle.Value.Length >= 13)
+                                {
+                                    
+                                }
+                                if (NoteTitle.Value.Length <= 13)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        {
 
                         }
-                        if (NoteTitle.Value == " ")
-                        {
-                            Console.WriteLine("Title can't be empty");
-                        }
-                        if (NoteTitle.Value == "")
-                        {
-                            Console.WriteLine("Title can't be empty");
-                        }
-                        if (NoteTitle.Value != "" && NoteTitle.Value != " ")
+                        //while (!string.IsNullOrEmpty(NoteTitle.Value))
+                        //{
+                        //    Console.WriteLine("Note can't be empty");
+                        //    Console.WriteLine("Enter Your desired title for note!");
+                        //    NoteTitle.Value = Console.ReadLine();
+                        //}
+                        if (NoteTitle.Value != "" || NoteTitle.Value != " ")
                         {
                             break;
                         }
                     }
                     Notes.Attributes.Append(NoteTitle);
 
-                    Console.WriteLine("Type your desired note.");
+                    Console.WriteLine("Write your desired note.");
                     NoteContent.Value = Console.ReadLine();
+                    //if (!string.IsNullOrEmpty(NoteContent.Value))
+                    //{
+                    //    Console.WriteLine("Note can't be empty");
+                    //}
+                    if (NoteContent.Value.Length >= 140)
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Note can't be longer than 140 letters");
+                            Console.WriteLine("Write your desired note.");
+                            NoteTitle.Value = Console.ReadLine();
+                            if (NoteTitle.Value.Length >= 140)
+                            {
+
+                            }
+                            if (NoteTitle.Value.Length <= 140)
+                            {
+                                break;
+                            }
+
+                        }
+                    }
                     Notes.Attributes.Append(NoteContent);
 
                     rootnode.AppendChild(Notes);
@@ -83,6 +163,7 @@ namespace XmlHarjutus
                             Console.WriteLine("Title " + xmlnode.Attributes["Title"].Value + ": ");
 
                             Console.WriteLine("Content: " + xmlnode.Attributes["Content"].Value);
+                            Console.WriteLine("\n");
 
                         }
                         Console.ReadLine();
